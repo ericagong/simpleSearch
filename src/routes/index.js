f523fe1f17a84543bf259b7283053ca7e559c31d
ericagong <48196721+ericagong@users.noreply.github.com>
2023-05-16 23:20:09 +0900

    feat: notFound 페이지 구현

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import { createRouter } from "../core/Router";
 import Home from "./Home";
 import Movie from "./Movie";
 import About from "./About";
+import NotFound from "./NotFound";
 
 /**
  * createRouter: 경로에 맞는 컴포넌트 객체 배열 반환
@@ -10,4 +11,5 @@ export default createRouter([
   { path: "#/", component: Home },
   { path: "#/movie", component: Movie },
   { path: "#/about", component: About },
+  { path: ".*", component: NotFound }, // 모든 정보와 매치되는 regex로 path 정보 대입 .* === .{0, }과 동일
 ]);
